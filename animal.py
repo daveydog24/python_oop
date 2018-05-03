84b278c5bc2801297ec77d25436edac5b7ff6a56
David Wukelic <36245241+daveydog24@users.noreply.github.com>
2018-05-02 23:05:45 -0700

    Add files via upload

---
+++ animal.py
@@ -0,0 +1,24 @@
+class Animal(object):
+    self.health
+    self.name
+
+    def health(self):
+        self.health
+        return self
+
+    def walk(self):
+        health -= 1
+        return self
+
+    def run(self):
+        health -= 5
+        return self
+
+    def displayHealth(self):
+        self.print(health)
+        return self
+
+
+    def Dog(object):
+
+    def Dragon(object):

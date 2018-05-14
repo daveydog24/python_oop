6886d4f2e6702081cef172dc764b0d8ad7dfe131
David Wukelic <36245241+daveydog24@users.noreply.github.com>
2018-05-13 23:16:31 -0700

    Add files via upload

---
+++ manage.py
@@ -0,0 +1,7 @@
+import product
+create_instance_product = product.Product(0.44, 'candy', .05, "nestle")
+print create_instance_product
+
+import store
+create_instance_store = store.Store('seattle', 'david')
+print create_instance_store
